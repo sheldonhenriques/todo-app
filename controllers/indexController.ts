3adda2c0155a83ff373d4ce69e7edc9f9fc700f5
sheldon <sheldonhenriques2727@gmail.com>
2023-03-18 18:01:08 +0530

    Avoid user from signing in again bug fix

--- controllers/indexController.ts
+++ controllers/indexController.ts
@@ -1,9 +1,13 @@
-import { Request, Response} from 'express';
+import { Request, Response } from "express";
+import _ from "lodash";
 
-const index = (req: Request, res: Response): void =>{
-    res.render('index')
-}
+const index = (req: Request, res: Response): void => {
+  if (_.get(req, "user")) {
+    return res.redirect(`/list/${_.get(req, ["user", "user_id"])}`);
+  }
+  res.render("index");
+};
 
 export default {
-    index
-}
+  index,
+};
--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,61 @@
-require('dotenv').config()
-import express from 'express'
-import  todoController from '../controllers/todoController'
-import  personController from '../controllers/personController'
-import  indexController from '../controllers/indexController'
-import  listController from '../controllers/listController'
-import  authenticateController from '../controllers/authenticateController'
-import { routes } from '../util/constants'
-import {verifyToken} from '../middleware/auth'
-import cookieParser from 'cookie-parser'
-
+require("dotenv").config();
+import express from "express";
+import todoController from "../controllers/todoController";
+import personController from "../controllers/personController";
+import indexController from "../controllers/indexController";
+import listController from "../controllers/listController";
+import authenticateController from "../controllers/authenticateController";
+import { routes } from "../util/constants";
+import { verifyToken, userLoggedIn } from "../middleware/auth";
+import cookieParser from "cookie-parser";
 
 // const urlencodedParser = bodyParser.json();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-app.use(express.static('./public'))
+app.use(express.static("./public"));
 
-app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded());
 app.use(cookieParser(process.env.COOKIE_SCERET));
 
-app.get(routes.index,indexController.index)
+app.get(routes.index, userLoggedIn, indexController.index);
 
-app.get(routes.todoId, verifyToken, todoController.getListItems)
+app.get(routes.todoId, verifyToken, todoController.getListItems);
 
-app.post(routes.todo, verifyToken, todoController.addListItem)
+app.post(routes.todo, verifyToken, todoController.addListItem);
 
-app.delete(routes.todoId, verifyToken, todoController.deleteListItem)
+app.delete(routes.todoId, verifyToken, todoController.deleteListItem);
 
 // User routes
-app.post(routes.signUp, personController.userSignUp)
+app.post(routes.signUp, personController.userSignUp);
 
-app.post(routes.login, authenticateController.login)
+app.post(routes.login, authenticateController.login);
 
-app.get(routes.profileId, personController.getUserProfile)
+app.get(routes.profileId, personController.getUserProfile);
 
-app.put(routes.profileId, personController.editUserProfile)
+app.put(routes.profileId, personController.editUserProfile);
 
-app.delete(routes.profileId, personController.deleteUserProfile)
+app.delete(routes.profileId, personController.deleteUserProfile);
 
 // List routes
-app.get(routes.listWithUserId, verifyToken, listController.getList)
+app.get(routes.listWithUserId, verifyToken, listController.getList);
 
-app.post(routes.list, verifyToken, listController.addList)
+app.post(routes.list, verifyToken, listController.addList);
 
-app.post(routes.listWithName, verifyToken, listController.searchListWithName)
+app.post(routes.listWithName, verifyToken, listController.searchListWithName);
 
-app.post(routes.listWithTag, verifyToken, listController.searchListWithTag)
+app.post(routes.listWithTag, verifyToken, listController.searchListWithTag);
 
-app.delete(routes.listId, verifyToken, listController.deleteList)
-
+app.delete(routes.listId, verifyToken, listController.deleteList);
 
 app.listen(port, () => {
-   // eslint-disable-next-line no-console
-   console.log({ level: "info", message: `Express is listening at http://localhost:${port}`})
-})
+  // eslint-disable-next-line no-console
+  console.log({
+    level: "info",
+    message: `Express is listening at http://localhost:${port}`,
+  });
+});
